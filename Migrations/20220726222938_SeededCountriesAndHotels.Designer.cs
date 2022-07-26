97065f3f3f99e6bb15d53db754488e82bcbe56a7
wolfpirker <w_pirker@gmx.de>
2022-07-27 00:55:49 +0200

    create seed data with migrations

--- Migrations/20220726222938_SeededCountriesAndHotels.Designer.cs
+++ Migrations/20220726222938_SeededCountriesAndHotels.Designer.cs
@@ -3,6 +3,7 @@ using HotelListingAPI.VSCode.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListingAPI.VSCode.Migrations
 {
     [DbContext(typeof(HotelListingDbContext))]
-    partial class HotelListingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220726222938_SeededCountriesAndHotels")]
+    partial class SeededCountriesAndHotels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
