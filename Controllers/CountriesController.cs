5554434bf69f70c06a8990ea691fab4d4d30ec2c
Wolfgang Pirker <wolfgang.pirker@hammermeister.biz>
2022-07-28 15:21:06 +0200

    recreate CountriesController in Visual Studio using API Controller with Actions and EntityFramework

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -1,8 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using HotelListingAPI.VSCode.Data;
 
 namespace HotelListingAPI.VSCode.Controllers
@@ -12,54 +14,94 @@ namespace HotelListingAPI.VSCode.Controllers
     public class CountriesController : ControllerBase
     {
         private readonly HotelListingDbContext _context;
+
         public CountriesController(HotelListingDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet("")]
-        public async Task<ActionResult<IEnumerable<Country>>> GetTModels()
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
         {
-            // TODO: Your code here
-            await Task.Yield();
-
-            return new List<Country> { };
+            return await _context.Countries.ToListAsync();
         }
 
+        // GET: api/Countries/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Country>> GetTModelById(int id)
+        public async Task<ActionResult<Country>> GetCountry(int id)
         {
-            // TODO: Your code here
-            await Task.Yield();
+            var country = await _context.Countries.FindAsync(id);
 
-            return null;
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
         }
 
-        [HttpPost("")]
-        public async Task<ActionResult<Country>> PostTModel(Country model)
-        {
-            // TODO: Your code here
-            await Task.Yield();
-
-            return null;
-        }
-
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTModel(int id, Country model)
+        public async Task<IActionResult> PutCountry(int id, Country country)
         {
-            // TODO: Your code here
-            await Task.Yield();
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
             return NoContent();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Country>> DeleteTModelById(int id)
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
         {
-            // TODO: Your code here
-            await Task.Yield();
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
 
-            return null;
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
         }
     }
 }
